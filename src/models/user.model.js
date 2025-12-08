b20a53e98247021d1daec624aa09356c97f3940c
KellianVK <kellianvk808@gmail.com>
2025-12-08 21:42:26 +0100

    SCRUM 16 Created user model

---
+++ src/models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+// User schema for authentication
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  displayName: {
+    type: String,
+  },
+});
+
+export default mongoose.model('User', userSchema);
