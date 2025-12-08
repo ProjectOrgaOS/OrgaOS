c62218fb7d48cf6d73328f610402a0535b7492d1
KellianVK <kellianvk808@gmail.com>
2025-12-08 22:06:43 +0100

    SCRUM-40: Add register and login routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import authRoutes from './routes/auth.routes.js';
 
 // Create the Express app
 const app = express();
 
 // Parse JSON bodies (for POST/PUT requests)
 app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 export default app;
